607939be8278e85143f6ec41e010abc09298b20a
sunny <sunnya2023@gmail.com>
2024-10-11 04:00:45 +0900

    completed Contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import "./contact.css";
+import SubHeader from "../../components/SubHeader";
+import HeaderImage from "../../images/header_bg_2.jpg";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <SubHeader title="Get in touch" image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit eum
+        tempore ullam officia animi ea veniam optio consequatur!
+      </SubHeader>
+
+      <section className="contact">
+        <div className="container contact__container">
+          <div className="contact__wrapper">
+            <a
+              href="mailto:sunnya2023@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <MdEmail />
+            </a>
+            <a
+              href="https://www.messenger.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsMessenger />
+            </a>
+            <a
+              href="https://web.whatsapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoLogoWhatsapp />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
